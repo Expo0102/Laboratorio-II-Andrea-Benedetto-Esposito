8ba59d57a5ebb66f5ded21fed22aa214d91af712
Expo0102 <a.esposito35@studenti.unipi.it>
2024-06-25 11:18:13 +0200

    consegna progetto

--- web-cnr.sol
+++
@@ -1,9 +0,0 @@
-Number of nodes: 325557
-Number of dead-end nodes: 86959
-Number of valid arcs: 3128710
-Converged after 114 iterations
-Sum of ranks: 1.0000   (should be 1)
-Top 3 nodes:
-  60595 0.025914
-  60597 0.025914
-  247028 0.005233
